d0778d178d265a3ee8d6d0a3e086d4459120b893
roamerwlop <18659108178@163.com>
2018-09-19 19:43:27 +0800

    2 change commit

--- src/main/java/SecondGit.java
+++ src/main/java/SecondGit.java
@@ -1,6 +1,6 @@
 public class SecondGit {
     public static void main(String[] args) {
         System.out.println("你所呢明知你不在还是会问!");
-        System.out.println();
+        System.out.println("修改变蓝色");
     }
 }
