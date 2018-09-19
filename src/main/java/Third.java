d3b8efb988f7e0905555b4e94e04bb8553f76566
roamerwlop <18659108178@163.com>
2018-09-19 19:44:11 +0800

    3 change commit

---
+++ src/main/java/Third.java
@@ -0,0 +1,10 @@
+public class Third {
+    public static void main(String[] args) {
+        System.out.println("没有提交的是红色");
+        System.out.println("修改的是蓝色");
+        System.out.println("修改代码之前需要pull代码到本地");
+        System.out.println("修改完成后再次pull");
+        System.out.println("没有问题就提交commit and push");
+
+    }
+}
