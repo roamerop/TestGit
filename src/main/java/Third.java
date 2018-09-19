f4154d3ec6915a4d8e16bda17d720c39a83b1ce2
roamerwlop <18659108178@163.com>
2018-09-19 19:49:29 +0800

    merge

--- src/main/java/Third.java
+++ src/main/java/Third.java
@@ -4,7 +4,7 @@ public class Third {
         System.out.println("修改的是蓝色");
         System.out.println("修改代码之前需要pull代码到本地");
         System.out.println("修改完成后再次pull");
-        System.out.println("没有问题就提交commit and push");
+        System.out.println("没有问题就提交commit and push ");
 
     }
 }
